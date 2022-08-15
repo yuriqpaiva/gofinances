9a0f620945031b185f99f541d2efa8d365d26711
Yuri <yuriqpaiva@gmail.com>
2022-08-15 18:38:10 -0300

    Dashboard styles using styled-components

---
+++ src/screens/Dashboard/index.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import { Container, Title } from './styles';
+
+export function Dashboard(): JSX.Element {
+  return (
+    <Container>
+      <Title>Dashboard</Title>
+    </Container>
+  );
+}

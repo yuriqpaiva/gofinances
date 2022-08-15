40dca593d60c24bd8b8d3820cb66449cc34d8e0c
Yuri <yuriqpaiva@gmail.com>
2022-08-15 19:07:49 -0300

    Add global theme

--- App.tsx
+++ App.tsx
@@ -1,6 +1,12 @@
 import React from 'react';
 import { Dashboard } from './src/screens/Dashboard';
+import { ThemeProvider } from 'styled-components';
+import theme from './src/global/styles/theme';
 
 export default function App(): JSX.Element {
-  return <Dashboard />;
+  return (
+    <ThemeProvider theme={theme}>
+      <Dashboard />
+    </ThemeProvider>
+  );
 }

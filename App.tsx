2eb8fda77c9a5c04fe4f97702ad95c9257de6835
Yuri <yuriqpaiva@gmail.com>
2022-08-15 21:34:27 -0300

    Add fonts

--- App.tsx
+++ App.tsx
@@ -1,12 +1,46 @@
-import React from 'react';
+import React, { useCallback, useEffect } from 'react';
 import { Dashboard } from './src/screens/Dashboard';
 import { ThemeProvider } from 'styled-components';
 import theme from './src/global/styles/theme';
+import {
+  Poppins_400Regular,
+  Poppins_500Medium,
+  Poppins_700Bold,
+  useFonts,
+} from '@expo-google-fonts/poppins';
+import * as SplashScreen from 'expo-splash-screen';
+import { View } from 'react-native';
 
-export default function App(): JSX.Element {
+export default function App(): JSX.Element | null {
+  const [fontsLoaded] = useFonts({
+    Poppins_400Regular,
+    Poppins_500Medium,
+    Poppins_700Bold,
+  });
+
+  useEffect(() => {
+    async function prepare(): Promise<void> {
+      await SplashScreen.preventAutoHideAsync();
+    }
+    prepare().finally(() => {});
+  }, []);
+
+  const onLayoutRootView = useCallback(async () => {
+    if (fontsLoaded) {
+      await SplashScreen.hideAsync();
+    }
+    return fontsLoaded;
+  }, [fontsLoaded]);
+
+  if (!fontsLoaded) {
+    return null;
+  }
+
   return (
     <ThemeProvider theme={theme}>
-      <Dashboard />
+      <View onLayout={onLayoutRootView} style={{ flex: 1 }}>
+        <Dashboard />
+      </View>
     </ThemeProvider>
   );
 }
--- src/global/styles/theme.ts
+++ src/global/styles/theme.ts
@@ -16,4 +16,9 @@ export default {
 
     background: '#F0F2F5',
   },
+  fonts: {
+    regular: 'Poppins_400Regular',
+    medium: 'Poppins_500Medium',
+    bold: 'Poppins_700Bold',
+  },
 };

0179f5f3f687d1a6dcfa05a3d935fbb8f5d7b7c9
Yuri <yuriqpaiva@gmail.com>
2022-08-20 16:46:05 -0300

    Add Resume Screen and History Card

--- src/routes/app.routes.tsx
+++ src/routes/app.routes.tsx
@@ -5,6 +5,7 @@ import { Register } from '../screens/Register';
 import { useTheme } from 'styled-components';
 import { Platform } from 'react-native';
 import { MaterialIcons } from '@expo/vector-icons';
+import { Resume } from '../screens/Resume';
 
 const { Navigator, Screen } = createBottomTabNavigator();
 
@@ -47,7 +48,7 @@ export function AppRoutes(): JSX.Element {
       />
       <Screen
         name="Resumo"
-        component={Register}
+        component={Resume}
         options={{
           tabBarIcon: ({ size, color }) => (
             <MaterialIcons name="pie-chart" size={size} color={color} />
